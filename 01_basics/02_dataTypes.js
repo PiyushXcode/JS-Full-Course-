ddde4f31c9fa29b1a48202f0cd1b641954d666bb
Piyush Prasad <piyushprasad0521@gmail.com>
2024-09-13 23:18:50 +0530

    config

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"Use Strict"; //treat all JS code as Newer Version 
+
+//alert(34+2);//we are using nodejs, not the browser
+
+console.log("Piyush");//this Is good Practice;
+
+
+console.log(3
+    + 3); //this is not the a proper way to wite Code tho it will give the output but also practice the Above way ; //code Readibiliy should be high;
+
+
+
+let name = "Piyush";
+let number = 2323;
+let isDeveloper = true;
+
+// like this Above data types there are differnt types 
+// number = 2 ^ 53
+// bigint = for large numbers
+// string = "assembly of characters/enclosed within Double Quotes"
+// boolean = true / false
+// null = stand alone value(type = object)
+// undefined = kindda place holder for a value which is not defined yet(type = undefined)
+// symbol = uniquness
+
+
+
+//Object 
+
+console.log(typeof null);//here the output will  be object as the null is decleared as object in JS;
+console.log(typeof undefined);//here the output will be undefined as it is the undefined data ;
+
+
