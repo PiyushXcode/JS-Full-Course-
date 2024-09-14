397df14e6826e1db4139b31fe8b0961247c008c7
Piyush Prasad <piyushprasad0521@gmail.com>
2024-09-14 12:04:19 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+// let score = "240";//output =>string;
+// let score = null;//output =>object;
+// let marks = "240gha";//output =>string;
+// let marks = undefined;//output =>undefined;
+// let score = true/false;//output =>boolean;
+// let marks = false;//output =>boolean;
+// let score = "Piyush";//output =>string;
+
+
+// console.log(typeof score);//method to display the data type .
+// console.log(typeof marks);//method to display the data type .
+
+// console.log(typeof (score));//method to display the data type .
+// console.log(typeof (marks));//method to display the data type .
+
+// let value_in_Number = Number(score);
+// let marks_in_Number = Number(marks);
+
+// console.log(typeof value_in_Number);
+// console.log(typeof marks_in_Number);
+// console.log(value_in_Number);
+// console.log(marks_in_Number);
+
+
+// "33" => string converts to number;
+//"33abc" => NaN (random mixer can't be converted into strings);
+//true => 1 | false => 0;
+
+
+// let isLoggedIn = 1;
+
+/*Another Method to Check which type of Data is Used;*/
+// let isBooleanLoggedIn = Boolean(isLoggedIn);
+// console.log(isBooleanLoggedIn);
+
+
+// console.log(Boolean(isLoggedIn)); 
+
+
+// let toCheck = "Piyush";
+// console.log(Boolean(toCheck));
+// let checked = Boolean(toCheck);
+// console.log(checked);
+
+//1 => true | 0 => false ;
+
+//" " => false | "Piyush" => true;
+
+
+// let SomeNumber = 22;
+
+// let StringCheck = String(SomeNumber);
+// console.log(StringCheck);
+// console.log(typeof StringCheck);
+
+
