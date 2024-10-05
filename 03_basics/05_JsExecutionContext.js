c1774f24354160890cc30621934d43dd139f30ef
Piyush Prasad <piyushprasad0521@gmail.com>
2024-10-06 00:51:12 +0530

    JEC

--- 03_basics/05_JsExecutionContext.js
+++ 03_basics/05_JsExecutionContext.js
@@ -1,5 +1,6 @@
 // Here All The Details Are Present in Written Formate ThereFore You Have to Access It From PC's Gallery Below Are the Links :-
 
+// Below Are Respectively:-
 
 // Link1:-c:\Users\Lenovo\Downloads\Phone Link\1728144628336.jpg 
 // Link2:-c:\Users\Lenovo\Downloads\Phone Link\1728144628349.jpg 
