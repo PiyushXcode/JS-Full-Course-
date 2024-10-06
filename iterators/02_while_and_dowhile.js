54bb68729f22a2717c367e36180613851ae477b7
Piyush Prasad <piyushprasad0521@gmail.com>
2024-10-06 23:15:00 +0530

    while anddo while

---
+++ iterators/02_while_and_dowhile.js
@@ -0,0 +1,22 @@
+//While And Do While Loops
+
+//Basic While Loop Structure;
+
+// iteration
+//  while (condition) {
+//     increment/decrement
+//  }
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(`Value of i is ${i}`);
+//     i++;
+// }
+
+
+
+let score = 17;
+do {
+    console.log(`Value of score is ${score}`);
+    score++;
+} while (score <= 10);
