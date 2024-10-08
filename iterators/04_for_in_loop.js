b27ebc0962cdca057c03c74385221eae2b9f1270
Piyush Prasad <piyushprasad0521@gmail.com>
2024-10-09 00:02:37 +0530

    for in Done

---
+++ iterators/04_for_in_loop.js
@@ -0,0 +1,25 @@
+let myObject = {
+    'js' : "JavaScript",
+    'py' : "Python",
+    'java' : "Java",
+    'rb' : "Rubby",
+    'cpp' : "C++",
+    'swift' : "Swift by Apple",
+}
+
+// for (const key in myObject) {
+//    console.log(key);// prints the key of Object; 
+// }
+
+
+// for (const key in myObject) {
+//     console.log(myObject[key]);   //print the Value of key 
+// }
+
+// for (const key in myObject) {
+//     console.log(`"${key}" for "${myObject[key]}"`); //prints Both the Keys And Value of Object;
+// }
+
+
+
+
